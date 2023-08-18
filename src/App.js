3dac08042bd393645c99d2c50b21b2c18d0015ff
Sarafaraj Shaikh <sarafarajshaikh@gmail.com>
2023-08-18 22:41:02 +0530

    main

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+//main part
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
+import Jumbotron from "../src/image/Logo.png";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <Navbar expand="lg" className="bg-dark" sticky="top" variant="dark">
+          <Container fluid>
+            <Navbar.Brand href="#">ResearchZeal</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+              <Nav
+                className="me-auto my-2 my-lg-0"
+                style={{ maxHeight: "100px" }}
+                navbarScroll
+              >
+                <Nav.Link href="#action1">Home</Nav.Link>
+                <Nav.Link href="#action2">Link</Nav.Link>
+                <NavDropdown title="Link" id="navbarScrollingDropdown">
+                  <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action4">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action5">
+                    Something else here
+                  </NavDropdown.Item>
+                </NavDropdown>
+                <Nav.Link href="#" disabled>
+                  Link
+                </Nav.Link>
+              </Nav>
+              <Form className="d-flex">
+                <Form.Control
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                />
+                <Button variant="outline-success">Search</Button>
+              </Form>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </header>
+      <main>
+        <Container>
+          <Row className="px-4 my-5">
+            <Col>
+              <Image
+                src={Jumbotron}
+                fluid
+                style={{ height: "auto", width: "100%" }}
+              />
+            </Col>
+          </Row>
+        </Container>
+      </main>
+    </div>
+  );
+}
+
+export default App;
