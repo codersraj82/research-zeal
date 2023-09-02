c067d429e3aefd516d65b770a9cebc53d03926e4
Sarafaraj Shaikh <sarafarajshaikh@gmail.com>
2023-09-02 19:23:34 +0530

    bottom nav bar

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,11 +8,13 @@ import Cards from "../cards/cards";
 import { cardData } from "../cards/cardData";
 import { Link } from "react-router-dom";
 import "../Home/Main.css";
-import Navbar from "../navbar/Navbar";
+
+import DriverCards from "../cards/DiverCards";
+import { driverData } from "../cards/driverData";
 
 function Home() {
   return (
-    <>
+    <div className="home-main-div">
       {/* <MainCarousel /> */}
       <div className="herosection">
         <div className="home-carousel">
@@ -22,10 +24,15 @@ function Home() {
           <RightSide />
         </div>
       </div>
+      <h3 className="card-h3">Hire Car</h3>
       <div className="card-container" id="hire-car">
         <Cards cardData={cardData} />
       </div>
-    </>
+      <h3 className="card-h3">Hire Driver</h3>
+      <div className="card-container" id="hire-car">
+        <DriverCards driverData={driverData} />
+      </div>
+    </div>
   );
 }
 
@@ -36,10 +43,10 @@ const RightSide = () => {
         <h2>Services</h2>
         <ul>
           <li>
-            <Link to="/hire-car">HireCar</Link>
+            <Link to="/hire-car">Hire Car</Link>
           </li>
           <li>
-            <Link to="/hire-driver">HireDriver</Link>
+            <Link to="/hire-driver">Hire Driver</Link>
           </li>
         </ul>
       </nav>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,50 +6,58 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Navbar = () => {
   const [showMenu, setShowMenu] = useState(false);
   return (
-    <nav className="navbar">
-      <a href="/" className="slide-title">
-        <h1>ResearchZeal</h1>
-      </a>
-      <button
-        className="toggle-button"
-        onClick={() => {
-          setShowMenu(!showMenu);
-        }}
-      >
-        <FontAwesomeIcon
-          icon="fa-solid fa-bars"
-          size="lg"
-          className="menu-icon"
-        />
-      </button>
-      <ul className="menus" id={showMenu ? "hidden" : ""}>
-        <li className="active-page">
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/hire-car">Hire Car</Link>
-        </li>
-        <li>
-          <Link to="/hire-driver">Hire Driver</Link>
-        </li>
-        <li>
-          <Link to="/about">About us</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-      </ul>
-      <div className="search-container">
-        <input
-          type="text"
-          name=""
-          id=""
-          className="searchbar"
-          placeholder="Search..."
-        />
-        <button>Search</button>
+    <>
+      <div className="mobile-header">
+        <a href="/" className="mobile-title">
+          <h1>ResearchZeal</h1>
+        </a>
+        {/* <button
+          className="toggle-button"
+          onClick={() => {
+            setShowMenu(!showMenu);
+          }}
+        >
+          <FontAwesomeIcon
+            icon="fa-solid fa-bars"
+            size="lg"
+            className="menu-icon"
+          />
+        </button> */}
       </div>
-    </nav>
+      <nav className="navbar">
+        <a href="/" className="slide-title">
+          <h1>ResearchZeal</h1>
+        </a>
+
+        <ul className="menus" id={showMenu ? "hidden" : ""}>
+          <li className="active-page">
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/hire-car">Hire Car</Link>
+          </li>
+          <li>
+            <Link to="/hire-driver">Hire Driver</Link>
+          </li>
+          <li>
+            <Link to="/about">About us</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+        <div className="search-container">
+          <input
+            type="text"
+            name=""
+            id=""
+            className="searchbar"
+            placeholder="Search..."
+          />
+          <button>Search</button>
+        </div>
+      </nav>
+    </>
   );
 };
 
